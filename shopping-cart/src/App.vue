e5c4e6f6d722391b3b610a23272ce1b768cee662
anastasia-starostina <anastasia.st.stern@gmail.com>
2023-05-11 15:12:26 +0100

    Add ProductCart component and finish the functionality of the pages

--- shopping-cart/src/App.vue
+++ shopping-cart/src/App.vue
@@ -1,33 +1,33 @@
 <template>
   <div>
-    <header class="top-bar spread">
-      <nav class="top-bar-nav">
-        <router-link to="/" class="top-bar-link">
-          <i class="icofont-spoon-and-fork"></i>
-          <span>Home</span>
-        </router-link>
-        <router-link to="/products" class="top-bar-link">
-          <span>Products</span>
-        </router-link>
-        <router-link to="/past-orders" class="top-bar-link">
-          <span>Past Orders</span>
-        </router-link>
-      </nav>
-      <div @click="toggleSidebar" class="top-bar-cart-link">
-        <i class="icofont-cart-alt icofont-1x"></i>
-        <span>Cart ({{ totalQuantity }})</span>
-      </div>
-    </header>
-    <router-view :inventory="inventory" />
+  <header class="top-bar spread">
+    <nav class="top-bar-nav">
+      <router-link to="/" class="top-bar-link">
+        <i class="icofont-spoon-and-fork"></i>
+        <span>Home</span>
+      </router-link>
+      <router-link to="/products" class="top-bar-link">
+        <span>Products</span>
+      </router-link>
+      <router-link to="/past-orders" class="top-bar-link">
+        <span>Past Orders</span>
+      </router-link>
+    </nav>
+    <div @click="toggleSidebar" class="top-bar-cart-link">
+      <i class="icofont-cart-alt icofont-1x"></i>
+      <span>Cart ({{ totalQuantity }})</span>
+    </div>
+  </header>
+  <router-view :inventory="inventory" :addToCart="addToCart" />
 
-    <SideBar
-      v-if="showSidebar"
-      :toggle="toggleSidebar"
-      :cart="cart"
-      :inventory="inventory"
-      :remove="removeItem"
-    />
-  </div>
+  <SideBar
+    v-if="showSidebar"
+    :toggle="toggleSidebar"
+    :cart="cart"
+    :inventory="inventory"
+    :remove="removeItem"
+  />
+</div>
 </template>
 
 <script>
@@ -53,10 +53,9 @@ export default {
     }
   },
   methods: {
-    addToCart (name, index) {
+    addToCart (name, quantity) {
       if (!this.cart[name]) this.cart[name] = 0
-      this.cart[name] += this.inventory[index].quantity
-      this.inventory[index].quantity = 0
+      this.cart[name] += quantity
     },
     toggleSidebar () {
       this.showSidebar = !this.showSidebar
